55eaaff8b7840c85975fcc74e478e7d5a350680e
Shannon Scirghio <shanscirg7@gmail.com>
2020-01-05 18:26:22 -0700

    added cards, moment.js for dates, and jquery to add current weather info

--- script.js
+++ script.js
@@ -8,8 +8,23 @@ $("#submitCity").on("click", function (event) {
         method: "GET"
     }).then(function (response) {
         console.log(response);
+        $("#cityName").text(response.name).append(" (" + moment().format('l') + ")");
+        let tempF = (response.main.temp - 273.15) * 1.80 + 32;
+        tempF = tempF.toFixed(2);
+        $("#temp").text("Temperature: " + tempF + " \xB0" + "F");
+        $("#humidity").text("Humidity: " + response.main.humidity + "%");
+        $("#wind").text("Wind Speed: " + response.wind.speed + " mph");
+        // $("#uvIndex").text("UV Index: " + response.)
     })
 });
+
+$("#nextDay1").text(moment().add(1,'days').format('l'));
+$("#nextDay2").text(moment().add(2,'days').format('l'));
+$("#nextDay3").text(moment().add(3,'days').format('l'));
+$("#nextDay4").text(moment().add(4,'days').format('l'));
+$("#nextDay5").text(moment().add(5,'days').format('l'));
+
+
 
 // const searchWeather = function (cityChoice) {
 
